72e66270957d15d4293506acb20cfb6e4f83e41c
Jian Zhang <184263567@qq.com>
2019-01-07 13:45:53 +0800

    check

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
-echo $_GET["echostr"];
+if (checkSignature())
+    echo $_GET["echostr"];
+else
+    echo -1;
+
+function checkSignature()
+{
+    $tmpArr = array(TOKEN, $_GET['timestamp'], $_GET["nonce"]);
+    sort($tmpArr, SORT_STRING);
+    $tmpStr = implode($tmpArr);
+    $tmpStr = sha1($tmpStr);
+
+    if ($_GET["signature"] == $tmpStr) {
+        return true;
+    } else {
+        return false;
+    }
+}
