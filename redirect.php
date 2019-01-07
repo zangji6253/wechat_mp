8b24c29919af5dc19b4b779178084e2eb25991ff
Jian Zhang <184263567@qq.com>
2019-01-07 13:13:45 +0800

    redirect

--- redirect.php
+++ redirect.php
@@ -6,7 +6,14 @@
  * Time: 下午1:04
  */
 
+include ("account.php");
 //echo 2;
 
 var_dump($_GET['code']);
 var_dump($_GET['state']);
+
+$url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid={$appID}&secret={$appsecret}&code={$_GET['code']}&grant_type=authorization_code";
+
+$r = geturl($url);
+
+var_dump($r);
