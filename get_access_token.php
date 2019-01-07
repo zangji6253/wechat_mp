8c05292fd8665a6ba6f920a517383ff474f0a6b2
Jian Zhang <184263567@qq.com>
2019-01-07 14:30:52 +0800

    token超时

--- get_access_token.php
+++ get_access_token.php
@@ -1,7 +1,19 @@
 <?php
-include ("curl.php");
-include ("account.php");
-$url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={$appID}&secret={$appsecret}";
+include("curl.php");
+include("account.php");
 
-$result = geturl($url);
-file_put_contents('access_token',$result['access_token']);
+function getAccessToken()
+{
+    $token = json_decode(file_get_contents('access_token.json'));
+
+    if (!$token || time() > $token->expires_time) {
+        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={APPID}&secret={APPSECRET}";
+
+        $result = geturl($url);
+        file_put_contents('access_token', json_encode(['access_token' => $result['access_token'], 'expires_time' => (time() + 3600)]));
+    } else {
+        return $token->access_token;
+    }
+}
+
+
--- get_menus.php
+++ get_menus.php
@@ -1,7 +1,8 @@
 <?php
 include 'curl.php';
+include 'get_access_token.php';
 
-$access_token=file_get_contents('access_token');
+$access_token = getAccessToken();
 
 //var_dump($access_token);
 $r = geturl("https://api.weixin.qq.com/cgi-bin/menu/get?access_token={$access_token}");
