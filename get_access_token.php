5cab499267815ea50bbfff58f25f3ac0d77a06d7
Jian Zhang <184263567@qq.com>
2019-01-07 14:38:53 +0800

    debug

--- get_access_token.php
+++ get_access_token.php
@@ -10,6 +10,7 @@ function getAccessToken()
         $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={APPID}&secret={APPSECRET}";
 
         $result = geturl($url);
+        var_dump($result);
         file_put_contents('access_token.json', json_encode(['access_token' => $result['access_token'], 'expires_time' => (time() + 3600)]));
 
         return $result['access_token'];
