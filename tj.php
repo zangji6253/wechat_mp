2958182d04b795ab74510085b22e073cfe61f785
Jian Zhang <184263567@qq.com>
2019-01-07 13:02:48 +0800

    urlencode

--- tj.php
+++ tj.php
@@ -3,6 +3,6 @@ include ("account.php");
 $REDIRECT_URI = urlencode("http://www.coderxx.com/redirect");
 $SCOPE = 'snsapi_base';
 $STATE = '123';
-var_dump("location:https://open.weixin.qq.com/connect/oauth2/authorize?appid={$appID}&redirect_uri={$REDIRECT_URI}&response_type=code&scope={$SCOPE}&state={$STATE}#wechat_redirect");
-exit;
+//var_dump("location:https://open.weixin.qq.com/connect/oauth2/authorize?appid={$appID}&redirect_uri={$REDIRECT_URI}&response_type=code&scope={$SCOPE}&state={$STATE}#wechat_redirect");
+//exit;
 header("location:https://open.weixin.qq.com/connect/oauth2/authorize?appid={$appID}&redirect_uri={$REDIRECT_URI}&response_type=code&scope={$SCOPE}&state={$STATE}#wechat_redirect");
